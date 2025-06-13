8edd42ef2c136594e3fb5321bf4c0deca94f407b
Vedant Rane <vedantrane81340@gmail.com>
2025-06-13 15:48:57 +0530

    travel entry created

---
+++ vehical transport system/TravelEntry.java
@@ -0,0 +1,51 @@
+//package com.vts;
+//
+//class TravelEntry {
+//    private String entryId;
+//    private String date;
+//    private double kilometers;
+//
+//    public TravelEntry(String entryId, String date, double kilometers) {
+//        this.entryId = entryId;
+//        this.date = date;
+//        this.kilometers = kilometers;
+//    }
+//    
+//    ///////////////////
+//    public double getKilometers() {
+//        return kilometers;
+//    }
+//////////////////////////////
+//
+//
+//    public void displayTravelEntry() {
+//        System.out.println("Travel Entry ID: " + entryId);
+//        System.out.println("Date: " + date);
+//        System.out.println("Kilometers: " + kilometers);
+//    }
+//}
+//
+
+package com.vts;
+
+public class TravelEntry {
+    private String entryId;
+    private String date;
+    private double kilometers;
+
+    public TravelEntry(String entryId, String date, double kilometers) {
+        this.entryId = entryId;
+        this.date = date;
+        this.kilometers = kilometers;
+    }
+
+    public double getKilometers() {
+        return kilometers;
+    }
+
+    public void displayTravelEntry() {
+        System.out.println("Travel Entry ID: " + entryId);
+        System.out.println("Date: " + date);
+        System.out.println("Kilometers: " + kilometers);
+    }
+}
